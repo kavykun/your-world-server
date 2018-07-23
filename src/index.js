fbb64142cde926a245e7c816e9ce797571e0d6f1
Kavy Rattana <kavyrattana@gmail.com>
2018-07-22 17:35:16 -0700

    Prepare files for Heroku deployment

--- src/index.js
+++ src/index.js
@@ -1,19 +1,41 @@
 const { GraphQLServer } = require('graphql-yoga')
 const { Prisma } = require('prisma-binding')
+const { ApolloEngine } = require('apollo-engine')
 const resolvers = require('./resolvers')
 
-const server = new GraphQLServer({
-  typeDefs: './src/schema.graphql',
+const prisma = new Prisma({
+  typeDefs: 'src/generated/prisma.graphql',
+  endpoint: process.env.PRISMA_ENDPOINT, // the endpoint of the Prisma DB service (value is set in .env)
+  secret: process.env.PRISMA_SECRET, // taken from database/prisma.yml (value is set in .env)
+  debug: true, // log all GraphQL queries & mutations
+});
+
+const graphQLServer = new GraphQLServer({
+  typeDefs: 'src/schema.graphql',
   resolvers,
   context: req => ({
     ...req,
-    db: new Prisma({
-      typeDefs: 'src/generated/prisma.graphql', // the auto-generated GraphQL schema of the Prisma API
-      endpoint: process.env.PRISMA_ENDPOINT, // the endpoint of the Prisma API (value set in `.env`)
-      debug: true, // log all GraphQL queries & mutations sent to the Prisma API
-      secret: process.env.PRISMA_SECRET, // only needed if specified in `database/prisma.yml` (value set in `.env`)
-    }),
+    db: prisma,
   }),
-})
+});
 
-server.start(() => console.log('Server is running on http://localhost:4000'))
+const port = parseInt(process.env.PORT, 10) || 4000;
+
+const engine = new ApolloEngine({
+  apiKey: process.env.APOLLO_ENGINE_API_KEY,
+});
+
+const httpServer = graphQLServer.createHttpServer({
+  tracing: true,
+  cacheControl: true,
+});
+
+engine.listen(
+  {
+    port,
+    httpServer,
+    graphqlPaths: ['/'],
+  },
+  () =>
+    console.log(`Server with Apollo Engine is running on http://localhost:${port}`)
+);
